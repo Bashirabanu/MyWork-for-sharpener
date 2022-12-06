315d7d42190cafa1c9abac3a2aa87cdc4ea62334
Bashirabanu <bashirajamadar1@gmail.com>
2022-12-06 21:59:05 +0530

    my new commit for tast 9

--- DOM/main.js
+++ DOM/main.js
@@ -1,11 +1,13 @@
 var form = document.getElementById('addForm');
 var itemList = document.getElementById('items');
+//var descriptionList = document.getElementById('description');
 var filter = document.getElementById('filter');
 
 // Form submit event
 form.addEventListener('submit', addItem);
 // Delete event
 itemList.addEventListener('click', removeItem);
+//descriptionList.addEventListener('click', removeItem);
 // Filter event
 filter.addEventListener('keyup', filterItems);
 
@@ -15,13 +17,17 @@ function addItem(e){
 
   // Get input value
   var newItem = document.getElementById('item').value;
+  var newDescription = document.getElementById('description').value;
 
   // Create new li element
   var li = document.createElement('li');
   // Add class
   li.className = 'list-group-item';
   // Add text node with input value
-  li.appendChild(document.createTextNode(newItem));
+  var newText = document.createTextNode(newItem);
+  var descriptionNode = document.createTextNode("   " +newDescription);
+  li.appendChild(newText);
+  li.appendChild(descriptionNode);
 
   // Create del button element
   var deleteBtn = document.createElement('button');
@@ -37,6 +43,7 @@ function addItem(e){
 
   // Append li to list
   itemList.appendChild(li);
+  //descriptionList.appendChild(li);
 }
 
 // Remove item
@@ -53,17 +60,20 @@ function removeItem(e){
 function filterItems(e){
   // convert text to lowercase
   var text = e.target.value.toLowerCase();
-  // Get lis
+  // Get list
   var items = itemList.getElementsByTagName('li');
+  //var descriptions = descriptionList.getElementsByTagName('li');
   // Convert to an array
   Array.from(items).forEach(function(item){
     var itemName = item.firstChild.textContent;
-    if(itemName.toLowerCase().indexOf(text) != -1){
+    var description = item.childNodes[1].textContent;
+    if(itemName.toLowerCase().indexOf(text) != -1 || description.toLocaleLowerCase().indexOf(text) != -1){
       item.style.display = 'block';
     } else {
       item.style.display = 'none';
     }
-  });
-}
 
+  });
+  
+}
 
