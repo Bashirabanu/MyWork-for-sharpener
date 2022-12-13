de83c3ae56780165d0ed2dc84c7666f6aa4e10ab
Bashirabanu <bashirajamadar1@gmail.com>
2022-12-13 13:38:43 +0530

    commit on task12

--- javascript file/event.js
+++ javascript file/event.js
@@ -20,28 +20,3 @@ btn.addEventListener('mouseout', (e) => {
     //document.querySelector('.items').lastElementChild.innerHTML ='<h1>hello</h1>';
     console.log('mouseout');
 });
-
-// const myForm = document.querySelector('#my-form');
-// const nameinput = document.querySelector('#name');
-// const emailInput = document.querySelector('#email');
-// const msg = document.querySelector('.msg');
-// const userList = document.querySelector('#users');
-
-// myForm.addEventListener('submit', onSubmit);
-
-// function onSubmit(e) {
-//     e.preventDefault();
-
-//     if(nameInput.value === '' || emailInput.value ===''){
-//         msg.classList.add('error');
-//         msg.innerHTML='please enter all fields';
-//         setTimeout(() => msg.remove(), 3000);
-//     }else {
-//         const li = document.createElement('li');
-//         li.appendChild(document.createTextNode(`${nameInput.value} : ${emailInput.value}`));
-
-//         userList.appendChild(li);
-//         nameInput.value = '';
-//         emailInput.value = '';
-//     }
-// }
